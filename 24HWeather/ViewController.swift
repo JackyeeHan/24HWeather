514af7f66f4264f47612b53c6d0f410d182cf916
Po Han Huang <jacky83522@gmail.com>
2022-06-28 18:10:24 +0800

    Initial Commit

---
+++ 24HWeather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  24HWeather
+//
+//  Created by 黃柏瀚 on 2022/6/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
